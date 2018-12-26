ad3a90d5b58c5e5d32baf59aeb422f0efb49613f
zrq2017 <1258303476@qq.com>
2018-12-26 21:06:33 +0800

    成功调用服务：处理zraapp调用不成功问题，删除dao层，解决datasource的同名占用

--- zrq-app/src/main/java/edu/soa/zrqapp/ZrqAppApplication.java
+++ zrq-app/src/main/java/edu/soa/zrqapp/ZrqAppApplication.java
@@ -1,15 +1,21 @@
 package edu.soa.zrqapp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @EnableDiscoveryClient
-@ComponentScan(basePackages = {"edu.soa.zrqapp.dao"})
 public class ZrqAppApplication {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ZrqAppApplication.class, args);
