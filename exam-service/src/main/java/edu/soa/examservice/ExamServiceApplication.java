3c2477d15fdc0e7ecec276b2d589680ab385ce33
zrq2017 <1258303476@qq.com>
2018-12-28 21:52:54 +0800

    食品管理系统改造删除，拆分erems系统，考试管理系统拆分比重过低，用户管理过高，考虑削减用户部分功能到考试管理

---
+++ exam-service/src/main/java/edu/soa/examservice/ExamServiceApplication.java
@@ -0,0 +1,16 @@
+package edu.soa.examservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ExamServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamServiceApplication.class, args);
+    }
+
+}
+
---
+++ user-manage/src/main/java/edu/soa/usermanage/UserManageApplication.java
@@ -0,0 +1,26 @@
+package edu.soa.usermanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication(exclude= DataSourceAutoConfiguration.class)
+@EnableEurekaClient
+public class UserManageApplication {
+    @Bean(name = "CustomerDataSource")
+    @ConfigurationProperties(prefix = "datasource.customer")
+    public DataSource testDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(UserManageApplication.class, args);
+    }
+
+}
+
